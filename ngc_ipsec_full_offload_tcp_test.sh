bc2e99d78cd5386340905baac381c637630a7dac
Leonid Bloch <lbloch@nvidia.com>
2023-07-24 11:38:30 +0300

    ipsec_full_offload: Flush IPsec settings in the end of the run
    Signed-off-by: Leonid Bloch <lbloch@nvidia.com>

--- ngc_ipsec_full_offload_tcp_test.sh
+++ ngc_ipsec_full_offload_tcp_test.sh
@@ -16,7 +16,10 @@ if [ -z "${MTU_SIZE}" ]; then
     net_name="$(ssh "${CLIENT_TRUSTED}" "ls -l /sys/class/infiniband/${CLIENT_DEVICE}/device/net/ | tail -1 | cut -d' ' -f9")"
     MTU_SIZE="$(ssh "${CLIENT_TRUSTED}" "ip addr | grep mtu | grep ${net_name} | cut -d' ' -f5")"
 fi
+
 scriptdir="$(dirname "$0")"
+source "${scriptdir}/ipsec_configuration.sh"
+
 bash "${scriptdir}/ipsec_full_offload_setup.sh" "${CLIENT_TRUSTED}" \
     "${CLIENT_DEVICE}" "${SERVER_TRUSTED}" "${SERVER_DEVICE}" "${LOCAL_BF}" \
     "${REMOTE_BF}" "$(( MTU_SIZE + 500 ))"
@@ -24,3 +27,6 @@ bash "${scriptdir}/ipsec_full_offload_setup.sh" "${CLIENT_TRUSTED}" \
 # Run tcp test
 bash "${scriptdir}/ngc_tcp_test.sh" "${CLIENT_TRUSTED}" "${CLIENT_DEVICE}" \
     "${SERVER_TRUSTED}" "${SERVER_DEVICE}" "HALF" "DONT_CHANGE"
+
+remove_ipsec_rules "${LOCAL_BF}"
+remove_ipsec_rules "${REMOTE_BF}"
