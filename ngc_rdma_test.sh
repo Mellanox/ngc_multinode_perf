d305522259cda182adcbbfce6d3144615c3971c7
Ravid Rothfeld <ravidro@mellanox.com>
2023-08-28 15:27:59 +0300

    Add devices for results
    Add mlx devices when echoing results

--- ngc_rdma_test.sh
+++ ngc_rdma_test.sh
@@ -195,8 +195,8 @@ for TEST in ib_write_bw ib_read_bw ib_send_bw ; do
 
     if $PASS
     then
-        log "NGC ${TEST} Passed"
+        log "NGC ${TEST} Passed for devices: ${SERVER_DEVICES[@]} <-> ${CLIENT_DEVICES[@]}"
     else
-        log "NGC ${TEST} Failed"
+        log "NGC ${TEST} Failed for devices: ${SERVER_DEVICES[@]} <-> ${CLIENT_DEVICES[@]}"
     fi
 done
